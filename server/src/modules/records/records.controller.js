df60f6b0426e4b1d135fc473b0127a136c6695fa
Victor Nkire <nkirevictor77@gmail.com>
2024-07-16 10:28:58 +0100

    code cleanup

--- server/src/modules/records/records.controller.js
+++ server/src/modules/records/records.controller.js
@@ -18,42 +18,75 @@ const httpCreateRecord = async (req, res) => {
   }
 };
 
+const testCreateRecord = async (req, res) => {
+  try {
+    const record = await recordsService.createRecord(req.body);
+
+    res.status(httpStatus.ACCEPTED).json(record);
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
 const httpGetRecords = async (req, res) => {
-    try {
-      const role = req.user.role;
-      if (role !== "Librarian") {
-        return res
-          .status(httpStatus.UNAUTHORIZED)
-          .json({ message: "Not Authorized" });
-      }
-
-      const record = await recordsService.getRecords();
-
-      res.status(httpStatus.OK).json(record);
-    } catch (error) {
-      res.status(httpStatus.BAD_REQUEST).json(error);
+  try {
+    const role = req.user.role;
+    if (role !== "Librarian") {
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json({ message: "Not Authorized" });
     }
-}
+
+    const record = await recordsService.getRecords();
+
+    res.status(httpStatus.OK).json(record);
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+const testGetRecords = async (req, res) => {
+  try {
+    const record = await recordsService.getRecords();
+
+    res.status(httpStatus.OK).json(record);
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
 
 const httpUpdateRecords = async (req, res) => {
-    try {
-      const role = req.user.role;
-      if (role !== "Librarian") {
-        return res
-          .status(httpStatus.UNAUTHORIZED)
-          .json({ message: "Not Authorized" });
-      }
-
-      const record = await recordsService.updateRecords(req.params.id, req.body);
-
-      res.status(httpStatus.ACCEPTED).json(record);
-    } catch (error) {
-      res.status(httpStatus.BAD_REQUEST).json(error);
+  try {
+    const role = req.user.role;
+    if (role !== "Librarian") {
+      return res
+        .status(httpStatus.UNAUTHORIZED)
+        .json({ message: "Not Authorized" });
     }
-}
+
+    const record = await recordsService.updateRecords(req.params.id, req.body);
+
+    res.status(httpStatus.ACCEPTED).json(record);
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+const testUpdateRecords = async (req, res) => {
+  try {
+    const record = await recordsService.updateRecords(req.params.id, req.body);
+
+    res.status(httpStatus.ACCEPTED).json(record);
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
 
 module.exports = {
-    httpCreateRecord,
-    httpGetRecords,
-    httpUpdateRecords
-}
+  httpCreateRecord,
+  httpGetRecords,
+  httpUpdateRecords,
+  testCreateRecord,
+  testGetRecords,
+  testUpdateRecords,
+};
--- server/src/modules/records/records.routes.js
+++ server/src/modules/records/records.routes.js
@@ -9,10 +9,27 @@ const {
 
 const RecordRoute = express.Router();
 
+
+//test Routes
+RecordRoute.route("/test")
+  .post(validate(createRecord), recordsController.testCreateRecord)
+  .get(recordsController.testGetRecords)
+  
+  RecordRoute.route("/test/:recordID").put(
+    validate(updateRecord),
+    recordsController.testUpdateRecords
+  );
+
+
+
 RecordRoute.route("/")
   .post(validate(createRecord), verifyToken, recordsController.httpCreateRecord)
   .get(verifyToken, recordsController.httpGetRecords)
-  .put(validate(updateRecord), verifyToken, recordsController.httpUpdateRecords);
 
+RecordRoute.route("/:recordID").put(
+  validate(updateRecord),
+  verifyToken,
+  recordsController.httpUpdateRecords
+);
 
 module.exports = RecordRoute;
